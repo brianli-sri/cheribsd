76dbbbdd5bfc96f078e6973aff1210d3864fffba
Konrad Witaszczyk <konrad.witaszczyk@cl.cam.ac.uk>
2022-12-09 14:02:19 +0000

    Register compressors with SYSINIT(9)
    Use a SYSINIT-based linker set rather than a regular linker set so that
    compressors can be registered at run time.

--- sys/dev/zlib/zlib_mod.c
+++ sys/dev/zlib/zlib_mod.c
@@ -250,7 +250,7 @@ static struct compressor_methods zlib_deflate_methods = {
 	.write = zlib_write,
 	.fini = zlib_fini
 };
-DATA_SET(compressors, zlib_deflate_methods);
+COMPRESSOR_LOAD(zlib_deflate, &zlib_deflate_methods);
 
 static struct compressor_methods zlib_inflate_methods = {
 	.format = COMPRESS_ZLIB_INFLATE,
@@ -259,7 +259,7 @@ static struct compressor_methods zlib_inflate_methods = {
 	.write = zlib_write,
 	.fini = zlib_fini
 };
-DATA_SET(compressors, zlib_inflate_methods);
+COMPRESSOR_LOAD(zlib_inflate, &zlib_inflate_methods);
 
 static int
 zlib_modevent(module_t mod, int type, void *unused)
