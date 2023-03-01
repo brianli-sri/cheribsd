8ab80b59962d2d23d04962382e9fe773ca6c1ba9
Konrad Witaszczyk <konrad.witaszczyk@cl.cam.ac.uk>
2023-03-01 11:25:20 +0000

    Introduce the module policy
    The module policy describes what compartmentalisation strategy should be
    used for a module.
    
    At the moment, it only includes a flag if a compartmentalisation
    mechanism should be enabled.

--- sys/sys/module.h
+++ sys/sys/module.h
@@ -38,6 +38,7 @@
 #define	MDT_MODULE	2		/* module declaration */
 #define	MDT_VERSION	3		/* module version(s) */
 #define	MDT_PNP_INFO	4		/* Plug and play hints record */
+#define	MDT_POLICY	5		/* module policy */
 
 #define	MDT_STRUCT_VERSION	1	/* version of metadata structure */
 #define	MDT_SETNAME	"modmetadata_set"
@@ -104,6 +105,14 @@ struct mod_pnp_match_info
 				/*   longer than descr describes). */
 	int num_entry;		/* Number of entries in the table */
 };
+
+/*
+ * Module policy declaration
+ */
+struct mod_policy {
+	bool	mp_compartment;
+};
+
 #ifdef	_KERNEL
 
 #include <sys/linker_set.h>
@@ -172,6 +181,15 @@ struct mod_pnp_match_info
 	};								\
 	MODULE_METADATA(MODULE_VERSION_CONCAT(module, version), MDT_VERSION,\
 	    &MODULE_VERSION_CONCAT(module, version), __XSTRING(module))
+
+#define	MODULE_POLICY_CONCAT(module)	_##module##_policy
+#define	MODULE_POLICY(module, compartment)				\
+	static struct mod_policy MODULE_POLICY_CONCAT(module)		\
+	    __section(".data") = {					\
+		compartment						\
+	};								\
+	MODULE_METADATA(MODULE_POLICY_CONCAT(module), MDT_POLICY,\
+	    &MODULE_POLICY_CONCAT(module), __XSTRING(module))
 
 /**
  * Generic macros to create pnp info hints that modules may export
